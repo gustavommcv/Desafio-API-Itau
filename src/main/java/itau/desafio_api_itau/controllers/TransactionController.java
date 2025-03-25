d18a1239d2757c1c9ad826095c4e5db160875235
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-03-25 19:51:47 -0300

    Update TransactionController.java
    Request & response requirements description for each controller method

--- src/main/java/itau/desafio_api_itau/controllers/TransactionController.java
+++ src/main/java/itau/desafio_api_itau/controllers/TransactionController.java
@@ -52,19 +52,42 @@ public class TransactionController {
         ));
     }
 
-    @GetMapping("/estatistica")
-    public ResponseEntity<Object> getEstatistica() {
-
-        return ResponseEntity.ok("post Transacao");
-    }
-
     @PostMapping("/transacao")
     public ResponseEntity<Object> postTransacao() {
+//        Request
+//        Tenham os campos valor e dataHora preenchidos
+//        A transação NÃO DEVE acontecer no futuro
+//        A transação DEVE ter acontecido a qualquer momento no passado
+//        A transação NÃO DEVE ter valor negativo
+//        A transação DEVE ter valor igual ou maior que 0 (zero)
+
+
+//        Response
+//        201 Created sem nenhum corpo
+//        A transação foi aceita (ou seja foi validada, está válida e foi registrada)
+//        422 Unprocessable Entity sem nenhum corpo
+//        A transação não foi aceita por qualquer motivo (1 ou mais dos critérios de aceite não foram atendidos - por exemplo: uma transação com valor menor que 0)
+//        400 Bad Request sem nenhum corpo
+//        A API não compreendeu a requisição do cliente (por exemplo: um JSON inválido)
         return ResponseEntity.ok("post Transacao");
     }
 
     @DeleteMapping("/transacao")
     public ResponseEntity<Object> deleteTransacao() {
+//        Response
+//        200 OK sem nenhum corpo
+//        Todas as informações foram apagadas com sucesso
+
         return ResponseEntity.ok("delete Transacao");
+    }
+
+    @GetMapping("/estatistica")
+    public ResponseEntity<Object> getEstatistica() {
+//        Response
+//        200 OK com os dados das estatísticas
+//        Um JSON com os campos count, sum, avg, min e max todos preenchidos com seus respectivos valores
+//        Atenção! Quando não houverem transações nos últimos 60 segundos considere todos os valores como 0 (zero
+        
+        return ResponseEntity.ok("get Estatistica");
     }
 }
